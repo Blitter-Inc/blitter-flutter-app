2908ae12db1803947ebb259baed3265095ae9b67
Pranav Jain <pranav19sept@gmail.com>
2022-01-11 10:18:44 +0530

    Moved root widget to lib/app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,8 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'firebase_options.dart';
 
+import './app.dart';
 import './data/blocs/blocs.dart';
-import './modules/auth/screens/signin_screen.dart';
-import './modules/home/screens/dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,34 +40,4 @@ void main() async {
           : await getApplicationDocumentsDirectory(),
     ),
   );
-}
-
-class BlitterApp extends StatelessWidget {
-  const BlitterApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Blitter',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      darkTheme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        primarySwatch: Colors.lightGreen,
-        colorScheme: const ColorScheme.dark(
-          primary: Colors.lightGreen,
-          primaryVariant: Colors.teal,
-        ),
-      ),
-      themeMode: ThemeMode.dark,
-      initialRoute: SigninScreen.route,
-      routes: {
-        SigninScreen.route: (_) => const SigninScreen(),
-        DashboardScreen.route: (_) => const DashboardScreen(),
-      },
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
